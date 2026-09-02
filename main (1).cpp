f9a410b7e643c4fbcb78b8e21944083f9cfe0ca7
yardelmaycolcconzaarpi-cmd <yardelmaycolcconzaarpi@gmail.com>
2026-09-02 09:22:04 -0500

    Add files via upload

---
+++ main (1).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    float radio, area;
+    const float PI = 3.14159;
+
+    cout << "\n Ingresa la base : ";
+    cin >> ;
+    
+    cout <<"ingrese la altura"
+    cin >> a
+    area = PI * radio * radio;
+
+    cout << " El area del circulo es: \n " << area << endl;
+
+    return 0;
+}
